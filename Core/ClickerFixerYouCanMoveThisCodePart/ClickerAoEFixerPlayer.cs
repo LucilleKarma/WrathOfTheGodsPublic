ca1de15a6e8052b8d104eb7e9933a43277d4f092
WitherTaco2 <133594341+WitherTaco2@users.noreply.github.com>
2025-02-26 12:04:22 +0300

    Clicker fixer prototype code

---
+++ Core/ClickerFixerYouCanMoveThisCodePart/ClickerAoEFixerPlayer.cs
@@ -0,0 +1,16 @@
+﻿using NoxusBoss.Content.NPCs.Bosses.Avatar.SecondPhaseForm;
+using Terraria.ModLoader;
+
+namespace NoxusBoss.Core.ClickerFixerYouCanMoveThisCodePart
+{
+    public class ClickerAoEFixerPlayer : ModPlayer
+    {
+        public override void UpdateEquips()
+        {
+            if (Player.isNearNPC(ModContent.NPCType<AvatarOfEmptiness>()))
+            {
+                ClickerCompat.EnableClickEffect(Player, "ClickerClass:PhaseReach");
+            }
+        }
+    }
+}
